6c812da5efbd8784631ab343b07f5d27b1fbe78e
danyortega1994-spec <danyortega_1994@hotmail.com>
2026-02-09 22:47:51 -0600

    Implement random number generation endpoint

--- PRACTICAS/Practica1/Practica1.js
+++ PRACTICAS/Practica1/Practica1.js
@@ -1,1 +1,20 @@
+//ALUMNO: Ortega Fuentes Daniel
+var express = require('express');
+var app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async function (request, response) {
+    response.json({'message':'Nothing to send'});
+});
+
+//PRACTICA 1, GENERAR UN NUMERO ALEATORIO:
+app.get('/aleatorio', async function (request, response) {
+    const numero = Math.floor(Math.random() * 100) + 1;
+    response.json({ "numero": numero });
+});
+
+app.listen(3000, function() {
+    console.log('Aplicación ejemplo, escuchando el puerto 3000!');
+});
